b94f7ec04cc2e630e71325d993038d60f6d5efad
Ray <raykeating13@gmail.com>
2023-01-02 11:41:59 -0500

    -

--- scripts/render-products.js
+++ scripts/render-products.js
@@ -1,5 +1,5 @@
 // fetch the watch data from our server
-const response = await fetch("/watches.json");
+const response = await fetch("/PrestigeWorldwide/watches.json");
 const watches = await response.json();
 
 // get the product-cards-grid element
@@ -16,6 +16,26 @@ for (let watch of watches) {
     card.querySelector(".product-card-brand").textContent = watch.brand;
     card.querySelector(".product-card-model").textContent = watch.model;
     card.querySelector(".product-card-image").src = watch.image;
+    card.querySelector("button").onclick = () => {
+        showProduct(watch);
+    };
     // add the card to the grid
     grid.appendChild(card);
+}
+
+const productOverlay = document.querySelector('.overlay');
+const closeProduct = document.querySelector('.close-product');
+
+closeProduct.onclick = () => {
+    productOverlay.classList.add("hidden");
+}
+
+const showProduct = (watch) => {
+    // set the data
+    productOverlay.querySelector(".product-brand").textContent = watch.brand;
+    productOverlay.querySelector(".product-model").textContent = watch.model;
+    productOverlay.querySelector(".product-image").src = watch.image;
+    productOverlay.querySelector(".product-price").textContent = `$${watch.price}`;
+    // show the overlay
+    productOverlay.classList.remove("hidden");
 }
